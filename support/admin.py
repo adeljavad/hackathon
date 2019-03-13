3a0d6f78a635d9c6d3fb6ee476f605d63b0e5db1
Javad Adel <adel.javad@elenoon.ir>
2019-03-13 12:17:14 +0330

    feat config admin site

--- support/admin.py
+++ support/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.db import connection
+
+from .models import (Pepole, QA,  )
+
+from django.contrib import admin
 
 # Register your models here.
+class pepoleAdmin(admin.ModelAdmin):
+
+    list_display = ('owner', 'user_type', 'first_name', 'last_name', 'user_id')
+    fieldsets = (
+        (None, {'fields': (
+            'owner', 'user_type', 'first_name', 'last_name', 'user_id',
+            ), }
+         ),)
+
+    list_filter = ['user_type']
+    search_fields = ['last_name']
+
+
+
+
+class QaAdmin(admin.ModelAdmin):
+    list_display = ('owner','qa_type','title', 'content', 'category')
+
+    fieldsets = (
+        (None, {'fields': ('owner','qa_type','title', 'content', 'category'), }
+         ),
+        ('اطلاعات اضافی', {
+            'classes': ('collapse',),
+            'fields': ('ref','pepole'),
+        }),
+    )
+
+    list_filter = ['qa_type', 'title']
+
+
+admin.site.register(QA, QaAdmin)
+admin.site.register(Pepole, pepoleAdmin)
